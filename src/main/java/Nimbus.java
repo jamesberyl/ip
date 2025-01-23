92ed3890711d083ddfd4fd06db2c6f41011a3dd2
Beryl <a.j.beryl@gmail.com>
2025-01-23 19:11:58 +0800

    Improve skeleton of Nimbus

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Nimbus {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Nimbus");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine(); // Read input
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + userInput); // Echo the user input
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
