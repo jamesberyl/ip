482dab07d3898a4b62e9700247c6e892179b0b48
Beryl <a.j.beryl@gmail.com>
2025-01-31 19:00:45 +0800

    Add Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,51 @@
+public class Parser {
+    private TaskList taskList;
+    private UI ui;
+    private Storage storage;
+
+    public Parser(TaskList taskList, UI ui, Storage storage) {
+        this.taskList = taskList;
+        this.ui = ui;
+        this.storage = storage;
+    }
+
+    public enum Command {
+        BYE, LIST, TODO, DEADLINE, EVENT, MARK, UNMARK, DELETE, FIND_DATE, CLEAR;
+
+        public static Command parseCommand(String input) throws NimbusException {
+            String command = input.split(" ")[0].toUpperCase();
+            try {
+                return Command.valueOf(command);
+            } catch (IllegalArgumentException e) {
+                throw new NimbusException("Oops! I don't recognize that command.");
+            }
+        }
+    }
+
+    public void processCommand(String input) throws NimbusException {
+        if (input.isEmpty()) {
+            throw new NimbusException("Oops! It seems like you entered nothing.");
+        }
+
+        Command command = Command.parseCommand(input);
+
+        switch (command) {
+            case BYE -> {
+                ui.showExitMessage();
+                return;
+            }
+            case LIST -> ui.showTaskList(taskList.getTasks());
+            case TODO -> taskList.addTodoTask(input);
+            case DEADLINE -> taskList.addDeadlineTask(input);
+            case EVENT -> taskList.addEventTask(input);
+            case MARK -> taskList.markTask(input, true);
+            case UNMARK -> taskList.markTask(input, false);
+            case DELETE -> taskList.deleteTask(input);
+            case FIND_DATE -> taskList.findTasksByDate(input);
+            case CLEAR -> taskList.clearAllTasks(ui);
+            default -> throw new NimbusException("Oops! I don't recognize that command.");
+        }
+
+        storage.saveTasks(taskList.getTasks());
+    }
+}
