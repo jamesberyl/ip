a92bf02f67817a6ff6bcb2ac9cd7081678d9315b
Beryl <a.j.beryl@gmail.com>
2025-02-06 02:01:51 +0800

    Create MainWindow class

---
+++ src/main/java/nimbus/MainWindow.java
@@ -0,0 +1,56 @@
+package nimbus;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Nimbus nimbus;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image nimbusImage = new Image(this.getClass().getResourceAsStream("/images/Nimbus.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Nimbus instance */
+    public void setNimbus(Nimbus n) {
+        nimbus = n;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = nimbus.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getNimbusDialog(response, nimbusImage)
+        );
+        userInput.clear();
+    }
+}
+
